564bf761fc6c8d9ff6739f9d6533c9d468c68e91
xuy1zhou <xuyg4583@gmail.com>
2022-11-17 17:09:33 +0900

    update

--- ECG_process.py
+++ ECG_process.py
@@ -6,11 +6,15 @@ import neurokit2 as nk
 import load_intan_rhd_format
 
 
-print(load_intan_rhd_format.read_data(r'D:\ECG\ECG002_221109_172137\info.rhd'))
+DataInfo = load_intan_rhd_format.read_data(r'D:\ECG\ECG002_221109_172137\info.rhd')
+SampleRate = DataInfo['frequency_parameters']['amplifier_sample_rate']
+StartPoint = int(1 * 60 * SampleRate)
+EndPoint = int(4 * 60 * SampleRate)
+
 with open(r'D:\ECG\ECG002_221109_172137\amplifier.dat', 'rb') as f:
     ECGfileData = np.fromfile(f, dtype=np.int16)
-ECGfileData = ECGfileData * 0.195
+ECGfileData = ECGfileData[StartPoint:EndPoint] * 0.195
 nkECGdataPeaks = nk.signal_findpeaks(ECGfileData, height_min=600)
-nkHRV = nk.hrv(nkECGdataPeaks, sampling_rate=20000)
-nkHR = nk.ecg_rate(nkECGdataPeaks, sampling_rate=20000)
-print(nkHRV, '\n', nkHR)
+nkFixedPeaks = nk.signal_fixpeaks(nkECGdataPeaks, sampling_rate=SampleRate)
+nkHRV = nk.hrv_time(nkFixedPeaks[1], sampling_rate=SampleRate)
+nkHR = np.asarray(nk.ecg_rate(nkFixedPeaks[1], sampling_rate=SampleRate), dtype=np.int16)
